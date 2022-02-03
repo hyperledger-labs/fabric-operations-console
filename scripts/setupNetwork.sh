e036cb57d3941dbb2d66bb5cbef519fe10965202
Varad Ramamoorthy <varad@us.ibm.com>
2022-02-03 17:31:46 -0500

    update ports
    Signed-off-by: Varad Ramamoorthy <varad@us.ibm.com>

--- scripts/setupNetwork.sh
+++ scripts/setupNetwork.sh
@@ -5,9 +5,10 @@ SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # Wh
 function networkUp() {
 	networkDown
 	cd $SRC_DIR/..
-	curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.4.1 1.5.2
+	curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.3 1.4.9
 	cd fabric-samples
-		cd test-network
+	git checkout release-2.2
+	cd test-network
 	./network.sh up createChannel -ca -c mychannel -s couchdb
 	./network.sh deployCC -ccn fabcar -ccv 1 -cci initLedger -ccl go -ccp ../chaincode/fabcar/go/
 }
