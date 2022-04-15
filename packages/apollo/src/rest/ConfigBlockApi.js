0ba57cb9ce7a2ab41e8f788b83ac4419f5d7b495
David Huffman <dshuffma@us.ibm.com>
2022-04-15 16:07:39 -0400

    clean up again
    Signed-off-by: David Huffman <dshuffma@us.ibm.com>

--- packages/apollo/src/rest/ConfigBlockApi.js
+++ packages/apollo/src/rest/ConfigBlockApi.js
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-import Logger from '../components/Log/Logger';
 import { RestApi } from './RestApi';
 
 class ConfigBlockApi {
