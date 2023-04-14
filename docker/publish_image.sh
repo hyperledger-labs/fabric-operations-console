b39cb5f7b14533c7d375b9c6793ea7abbb38999c
dshuffma-ibm <dshuffma@us.ibm.com>
2023-04-14 13:01:13 -0400

    bump node to 18 in dockerfile & add tag to image (#452)
    Signed-off-by: David Huffman <dshuffma@us.ibm.com>

--- docker/publish_image.sh
+++ docker/publish_image.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 IMAGE_BUILD_NAME=fabric-console:latest
-# Info about the build is saved in tags on the docker image
-GIT_TAG=$(git describe | grep -v -)
 
+# Info about the build is saved in tags on the docker image
+GIT_TAG=$(git describe --tags)
+echo "Found tag $GIT_TAG"
+
+# Push the images
+echo "Pushing 'latest' image:"
 echo ${GITHUB_TOKEN} | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
 docker push ghcr.io/hyperledger-labs/fabric-console:latest
+
+# -n means if GIT_TAG's length is greater than 0
 if [[ -n $GIT_TAG ]]; then
+	echo "Pushing tagged image $GIT_TAG:"
 	docker push ghcr.io/hyperledger-labs/fabric-console:${GIT_TAG}
 fi
