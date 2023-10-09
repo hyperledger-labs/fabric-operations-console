52162187a87ce05bc1b52a5c491dbaf7414b219e
Ketul Shah <shah.ketul@ibm.com>
2023-10-09 18:50:19 +0530

    Cypress UI automated test cases of console
    Signed-off-by: Ketul Shah <shah.ketul@ibm.com>

---
+++ packages/cypress/cypress/e2e/features/8-InstallInstantiate1xSmartContract.feature
@@ -0,0 +1,32 @@
+@support @regression
+Feature: 1.x Smart Contract flow
+
+    Background: Login to console
+        Given I go to the console
+        And I am logged in
+        And I am ready to get started
+        And I am on the 'smart_contracts' page
+
+    Scenario: Install 1.x Smart Contract on Org1 and Org2 peers
+        And I clicked the button with title 'Install smart contract'
+        And I upload file 'cypress/fixtures/test_data/chaincodes/fabcar_go_2.1.1.cds' to 'file-uploader-cds' input
+        And I clicked the button with text 'Next'
+        And I click label with property 'for' and value 'installChaincode-peer-peerorg1'
+        And I click label with property 'for' and value 'installChaincode-peer-peerorg2'
+        And I clicked the button with id 'submit'
+        Then wait "20" seconds
+        Then I should see button with id 'overflow-installed-fabcar_2.1.1'
+
+    Scenario: Instantiate 1.x Smart Contract on channel1
+        And I clicked the button with id 'overflow-installed-fabcar_2.1.1'
+        And I clicked the button with id 'instantiate_modal'
+        And I clicked the button with title 'Select channel'
+        And I clicked the div with text 'channel1'
+        And I clicked the button with id 'next'
+        And I clicked the button with id 'next'
+        And I clicked the button with id 'next'
+        And I clicked the button with text 'Instantiate smart contract'
+        Then wait "10" seconds
+        Then I should see button with id 'overflow-installed-fabcar_2.1.1'
+		Then wait "5" seconds
+        Then I should see a smart contract 'fabcar' with version '2.1.1' in channel 'channel1'
---
+++ packages/cypress/cypress/support/step_definitions/channel_steps.js
@@ -0,0 +1,63 @@
+/*
+ * Copyright contributors to the Hyperledger Fabric Operations Console project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+import { Given, Then, When } from "@badeball/cypress-cucumber-preprocessor";
+
+Given(/^I clicked the (?:'|")(.*?)(?:'|") role for (?:'|")(.*?)(?:'|")$/, (role, mspId) => {
+	cy.get(`label[for="ibp-add-orgs-msp-${mspId}-role-${role}"]`).click()
+});
+
+Then(/^the channel with name (?:'|")(.*?)(?:'|") should have been created$/, (channelName) => {
+	cy.get('.ibp-container-tile-pending').contains(channelName).should('be.visible')
+});
+
+Then(/^I clicked Create channel button$/, () => {	
+	cy.wait(500)
+	cy.get('#ibp--template-full-page-side-panel > div.ibp-button-container.ibp-vertical-panel-button-container > button:nth-child(2) > span').click()
+  });
+
+Then(/^the chaincode with name (?:'|")(.*?)(?:'|") should have been created in (?:'|")(.*?)(?:'|") state$/, (chaincodeName, state) => {
+	try {
+		cy.get('.ibp-tile-content-title').contains(chaincodeName).should('be.visible')
+		cy.get('.ibp-channel-chaincode-status').contains(state).should('be.visible')
+	} catch (err) {
+		cy.log('Error: ', err)
+	}
+});
+
+Then(/^I should see a smart contract (?:'|")(.*?)(?:'|") with version (?:'|")(.*?)(?:'|") in channel (?:'|")(.*?)(?:'|")$/, (name, version, channel) => {
+	try {
+		cy.get('table[id="table-instantiated_chaincode"]').find("tr").then((rows) => {
+			rows.toArray().forEach((element) => {
+				if (element.innerText.includes(name)) {
+					if (element.innerText.includes(version)){
+						if (element.innerText.includes(channel)){
+							cy.log('Found smart contract:', element.innerText)
+						}
+					}
+				}
+			});
+		});
+		cy.get('#instantiated-chaincode-channel-0').contains(channel).should('be.visible')
+	} catch (err) {
+		cy.log('Error: ', err)
+	}
+});
+
+Then(/^I should see Create Genesis screen with message to to join orderer$/, () => {
+	cy.wait(1000)
+	cy.get('.ibp-channel-section-genesis').should('be.visible')
+});
