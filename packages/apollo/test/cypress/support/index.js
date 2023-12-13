027da576bdb46afc2cf7c6bfb2971ae839115307
Ketul Shah <shah.ketul@ibm.com>
2023-12-13 17:06:47 +0530

    uncaught exception not from Cypress but causes test to fail
    Signed-off-by: Ketul Shah <shah.ketul@ibm.com>

--- packages/apollo/test/cypress/support/index.js
+++ packages/apollo/test/cypress/support/index.js
@@ -1,5 +1,8 @@
 import './commands'
 Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from failing the test
-  return false
-})
+	if (err.message.includes('member defines an octet string of length 31 bytes but should be 32')) {
+	  return false
+	}
+	// we still want to ensure there are no other unexpected
+	// errors, so we let them fail the test
+  })
