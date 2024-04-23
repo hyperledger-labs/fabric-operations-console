17d6d2d0ff86c595798499e4a16244217a0883d8
Chandra Kant Paliwal <paliwal.ck+ibm@gmail.com>
2024-04-23 14:24:47 +0530

    remove linters
    Signed-off-by: Chandra Kant Paliwal <paliwal.ck+ibm@gmail.com>

--- packages/apollo/src/utils/i18n.js
+++ packages/apollo/src/utils/i18n.js
@@ -1,15 +1,9 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
 import defaultTranslation from '../assets/i18n/en/messages.json';
 
 i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
-//   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
   .init({
     debug: false,
     fallbackLng: 'en',
