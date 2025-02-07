1ac4356158dfe378c257a229ae79b43bda8eacbb
Muthu <muthu.sundaravadivel@in.ibm.com>
2025-02-07 17:10:17 +0530

    Woekflow Changes

--- packages/fabric-deployer/scripts/build_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-
-## clone fabric-deployer repo from opensource
-git clone https://github.com/hyperledger-labs/fabric-operations-console.git --depth 1 --branch ${TAG}
-cp -r ./fabric-operations-console/packages/fabric-deployer .
-cd fabric-deployer
-go build -o /tmp/build/_output/bin/deployer .
