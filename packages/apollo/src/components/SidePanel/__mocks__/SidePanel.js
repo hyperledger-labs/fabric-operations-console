168f6a69b728e822ab5b33466b8c57c356b9c181
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-04-17 18:46:53 +0300

    Update SidePanel.js
    Signed-off-by: leopardracer <136604165+leopardracer@users.noreply.github.com>

--- packages/apollo/src/components/SidePanel/__mocks__/SidePanel.js
+++ packages/apollo/src/components/SidePanel/__mocks__/SidePanel.js
@@ -19,7 +19,7 @@
 /**
  * this file is used to return a mock implementation of the SidePanel component
  * elsewhere we use inline jest mocks to mock unwanted child components, but this wasn't possible with SidePanel due to its use of refs
- * it's not possible to pass refs to functional componenets without using React.forwardRef, but jest doesn't allow inline references to external modules
+ * it's not possible to pass refs to functional components without using React.forwardRef, but jest doesn't allow inline references to external modules
  * instead we create an actual React component in a separate mock file and replace SidePanel in the test files
  */
 
